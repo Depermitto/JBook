f5e413f54ced0905f07606542768ea955a21de34
Depermitto <penciller@disroot.org>
2025-04-01 23:48:37 +0200

    rewrite into Java

---
+++ src/Actions.java
@@ -0,0 +1,116 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class Actions {
+    public static void err(String msg) {
+        System.err.printf("\u001b[91merror:\u001b[0m " + msg + '\n');
+    }
+
+    public static void warn(String msg) {
+        System.err.printf("\u001b[33mwarning:\u001b[0m " + msg + '\n');
+    }
+
+    public static int pipeline(CommandLine cmd) {
+        var args = cmd.getArgList();
+        assert !args.isEmpty();
+
+        var file = new File(args.get(0));
+        var filename = file.getName();
+        try (PDDocument doc = Loader.loadPDF(file)) {
+            var pages = doc.getPages();
+            if ((doc.getNumberOfPages() & 1) == 1) doc.addPage(new PDPage(pages.get(0).getMediaBox()));
+
+            List<Integer> indices = Collections.emptyList();
+            if (cmd.hasOption('b')) indices = simplex(doc.getNumberOfPages());
+            else if (cmd.hasOption('B')) indices = duplex(doc.getNumberOfPages());
+
+            var booklet = new PDDocument();
+            for (int i : indices) {
+                booklet.addPage(pages.get(i - 1));
+            }
+
+            var path = Files.createTempFile(filename.substring(0, filename.lastIndexOf('.')) + '.', ".tmp.pdf");
+            booklet.save(path.toFile());
+            booklet.close();
+
+            if (cmd.hasOption('q')) {
+                System.out.println("file available at " + path.toUri());
+                return 0;
+            }
+            if (Desktop.isDesktopSupported()) {
+                var desktop = Desktop.getDesktop();
+                if (desktop.isSupported(Desktop.Action.BROWSE)) {
+                    desktop.browse(path.toUri());
+                    return 0;
+                }
+            }
+            try {
+                var process = new ProcessBuilder("xdg-open", path.toAbsolutePath().toString()).inheritIO().start();
+                int exitCode = process.waitFor();
+                if (exitCode != 0) {
+                    warn("cannot open booklet in web browser, file available at " + path.toUri());
+                }
+            } catch (IOException | InterruptedException e) {
+                warn("opening booklet in web browser interrupted, file available at " + path.toUri());
+            }
+        } catch (IOException ioe) {
+            err(filename + " does not exist");
+            return 1;
+        }
+        return 0;
+    }
+
+    public static List<Integer> duplex(int n) {
+        var pages = new ArrayList<Integer>(n);
+        for (int i = 0; i < n / 2; i++) {
+            if ((i & 1) == 0) {
+                pages.add(n - i);
+                pages.add(i + 1);
+            } else {
+                pages.add(i + 1);
+                pages.add(n - i);
+            }
+        }
+        if ((n & 1) == 1) {
+            pages.add((n + 1) / 2);
+        }
+        return pages;
+    }
+
+    @Test
+    public void testDuplex() {
+        assert duplex(4).equals(List.of(4, 1, 2, 3));
+        assert duplex(8).equals(List.of(8, 1, 2, 7, 6, 3, 4, 5));
+        assert duplex(15).equals(List.of(15, 1, 2, 14, 13, 3, 4, 12, 11, 5, 6, 10, 9, 7, 8));
+        assert duplex(17).equals(List.of(17, 1, 2, 16, 15, 3, 4, 14, 13, 5, 6, 12, 11, 7, 8, 10, 9));
+    }
+
+    public static List<Integer> simplex(int n) {
+        var pages = duplex(n);
+        for (int i = 1; i < n; i += 4) {
+            if (i + 1 < n) {
+                Collections.swap(pages, i, i + 1);
+            }
+        }
+        return pages;
+    }
+
+    @Test
+    public void testSimplex() {
+        assert simplex(4).equals(List.of(4, 2, 1, 3));
+        assert simplex(8).equals(List.of(8, 2, 1, 7, 6, 4, 3, 5));
+        assert simplex(15).equals(List.of(15, 2, 1, 14, 13, 4, 3, 12, 11, 6, 5, 10, 9, 8, 7));
+        assert simplex(17).equals(List.of(17, 2, 1, 16, 15, 4, 3, 14, 13, 6, 5, 12, 11, 8, 7, 10, 9));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,98 @@
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.Loader;
+import org.apache.commons.cli.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
 
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.err.println("\u001b[91mMissing required parameter: 'PDF file'\u001B[0m\n" +
-					"Usage: pbook <PDF file>\n" +
-					"Version: 1.0");
-			System.exit(1);
-		}
+    public static void main(String[] args) {
+        final Properties properties = new Properties();
+        try {
+            properties.load(Actions.class.getClassLoader().getResourceAsStream(".properties"));
+        } catch (IOException e) {
+            throw new RuntimeException("Unreachable");
+        }
 
-		Logger.getLogger("org.apache.pdfbox").setLevel(Level.OFF);
+        var options = new Options()
+//                .addOption(Option.builder("output")
+//                        .option("m")
+//                        .hasArgs()
+//                        .argName("file")
+//                        .desc("merge all input documents into <file>")
+//                        .build())
+                .addOption(Option.builder("b")
+                        .option("b")
+                        .desc("rearrange pages to create a booklet view")
+                        .build())
+                .addOption(Option.builder("B")
+                        .option("B")
+                        .desc("same as 'b' but for duplex printers")
+                        .build())
+                .addOption(Option.builder("quiet")
+                        .option("q")
+                        .desc("do not open booklet in the web-browser")
+                        .build())
+                .addOption(Option.builder()
+                        .option("h")
+                        .desc("print this message")
+                        .build())
+                .addOption(Option.builder("version")
+                        .option("v")
+                        .desc("print program version")
+                        .build());
 
-		var file = new File(args[0]);
-		if (!file.exists()) {
-			System.err.printf("\u001b[91mError:\u001b[0m '%s' does not exist\n", file.getName());
-			System.exit(1);
-		}
+        Runnable help = () ->
+                new HelpFormatter().printHelp(properties.getProperty("name") + " <files...>", options);
 
-		try (var doc = Loader.loadPDF(file)) {
-			var booklet = new PDDocument();
-			var bookletIndices = duplex(doc.getNumberOfPages());
-			var pages = doc.getPages();
-			for (int i : bookletIndices) {
-				booklet.addPage(pages.get(i - 1));
-			}
-			booklet.save("booklet.pdf");
-			booklet.close();
-		} catch (IOException ioe) {
-			System.err.printf("\u001b[91mError:\u001b[0m '%s' is not a PDF file\n", file.getName());
-			System.exit(1);
-		}
-	}
+        try {
+            CommandLine cmd = new DefaultParser().parse(options, args);
+            if (cmd.hasOption('h')) {
+                help.run();
+                return;
+            }
 
-	public static List<Integer> duplex(int n) {
-		var pages = new ArrayList<Integer>(n);
-		for (int p = 0; p < n / 2; p++) {
-			if ((p & 1) == 1) {
-				pages.add(p + 1);
-				pages.add(n - p);
-			} else {
-				pages.add(n - p);
-				pages.add(p + 1);
-			}
-		}
-		if ((n & 1) == 1) {
-			pages.add((n + 1) / 2);
-		}
-		return pages;
-	}
+            if (cmd.hasOption('v')) {
+                System.out.println(properties.getProperty("version"));
+                return;
+            }
+
+            if (cmd.getArgs().length == 0) {
+                help.run();
+                System.exit(1);
+            }
+
+            if (!cmd.hasOption('b') && !cmd.hasOption('B')) {
+                Actions.err("you must specify either the '-b' or '-B' option");
+                System.exit(2);
+            }
+
+            if (cmd.hasOption('b') && cmd.hasOption('B')) {
+                Actions.err("cannot use both '-b' and '-B' options together");
+                System.exit(3);
+            }
+
+            Logger.getLogger("org.apache.pdfbox").setLevel(Level.OFF);
+            System.exit(Actions.pipeline(cmd));
+        } catch (ParseException ignore) {
+            help.run();
+            System.exit(4);
+        }
+//        var pdfMergerUtility = new PDFMergerUtility();
+//        for (int i = 1; i < cmd.getArgList().size(); i++) {
+//            var file = new File(cmd.getArgList().get(i));
+//            filename = file.getName();
+//            if (!file.exists()) {
+//                System.err.printf("\u001b[91mError:\u001b[0m '%s' does not exist, skipping...\n", filename);
+//                continue;
+//            }
+//
+//            try (var doc = Loader.loadPDF(file)) {
+//                pdfMergerUtility.appendDocument(merge, doc);
+//            } catch (IOException ioe) {
+//                System.err.printf("\u001b[91merror:\u001b[0m '%s' is not a PDF file, skipping...\n", filename);
+//            }
+//        }
+    }
 }
